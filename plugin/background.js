8eac3ed6d2febcdc46f673f25fbf4893218b3dfe
Charles Marsh <crmarsh416@gmail.com>
2013-10-05 22:42:54 -0400

    icon glow effect

---
+++ plugin/background.js
@@ -0,0 +1,36 @@
+var updateIcon = function(url) {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+
+    if (xmlhttp.responseText == "True") {
+        chrome.browserAction.setIcon({path: 'glow.png'});
+    } else {
+        chrome.browserAction.setIcon({path: 'icon.png'})
+    }
+}
+
+var update = function(tabId, changeInfo, tab) {
+    var url = "http://localhost:8000/fbexists?url=" + encodeURIComponent(tab.url);
+
+    chrome.tabs.executeScript({
+        code: 'console.log("' + url + '")'
+    });
+
+    updateIcon(url);
+}
+
+var activate = function(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        var url = "http://localhost:8000/fbexists?url=" + encodeURIComponent(tab.url);
+
+        chrome.tabs.executeScript({
+            code: 'console.log("' + url + '")'
+        });
+
+        updateIcon(url);
+    });
+}
+
+chrome.tabs.onUpdated.addListener(update);
+chrome.tabs.onActivated.addListener(activate);
